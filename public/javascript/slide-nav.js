e91b798a07ecc75df56c486251745eacae1a90ea
famaeoxiaa <faisaltaslim79@gmail.com>
2025-12-13 23:00:58 +0530

    Initial launch of wearable Lux

---
+++ public/javascript/slide-nav.js
@@ -0,0 +1,7 @@
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.main-nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
